0bbc5e714540504aee925e503a8187e0eda1917a
MedhaVashisth <112161904+MedhaVashisth@users.noreply.github.com>
2022-10-03 17:45:43 +0530

    Add files via upload

---
+++ Advanced Assignment_Python 7.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. The try block lets you test a block of code for errors. The except block lets you handle the error. The else block lets you execute code when there is no error.
+
+2. The Different Try/Except Variations. So far we've used a try / except and even a try / except / except , but this is only two-thirds of the story. There are two other optional segments to a try block: else and finally . Both of these optional blocks will come after the try and the except 
+
+3. The raise keyword is used to raise an exception. You can define what kind of error to raise, and the text to print to the user.
+
+4. In simpler terms, we can say that assertion is the boolean expression that checks if the statement is True or False. If the statement is true then it does nothing and continues the execution, but if the statement is False then it stops the execution of the program and throws an error
+
+5. The with statement simplifies our write process to just two lines. Here, with is used to query an SQLite database and return its content.
